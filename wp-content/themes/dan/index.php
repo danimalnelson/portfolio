0226b5ec296dcaec290e532b89e36533fa3d0812
Dan <dannelson@icloud.com>
2014-07-09 21:55:59 -0500

    Minor edits

--- wp-content/themes/dan/index.php
+++ wp-content/themes/dan/index.php
@@ -3,7 +3,7 @@
 			<section id="about-dan">
 				<h1>Dan Nelson</h1>
 				<p class="intro-paragraph">I build customer-facing products in Chicago. Currently at <a href="https://www.trunkclub.com/"target="_blank">Trunk Club</a> as Lead Product Designer. Previously at <a href="http://www.mademovement.com/"target="_blank">Made Movement</a>.</p>
-				<p class="intro-paragraph">I'm also co-owner of <a href="http://therubytap.com/" target="_blank">The Ruby Tap</a>, a wine bar in Milwaukee, with my wife Sarah. Follow me on <a href="https://twitter.com/danimalnelson" target="_blank">Twitter</a> and connect with me through <a href="mailto:dannelson@icloud.com">email</a>.</p>
+				<p class="intro-paragraph">Along with my wife Sarah, I'm also co-owner of <a href="http://therubytap.com/" target="_blank">The Ruby Tap</a>, a wine bar in Milwaukee. Follow me on <a href="https://twitter.com/danimalnelson" target="_blank">Twitter</a> and connect with me through <a href="mailto:dannelson@icloud.com">email</a>.</p>
 				<div id="logo">Dan Nelson Logo</div>
 			</section>
 
