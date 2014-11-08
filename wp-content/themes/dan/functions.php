27c6700dfb87c25c4a974d871d63da532a57ae3b
Dan <dannelson@icloud.com>
2014-11-08 16:37:37 -0600

    Site redesign

--- wp-content/themes/dan/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once( 'external/starkers-utilities.php' );
-	add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
-	add_filter( 'body_class', array( 'Starkers_Utilities', 'add_slug_to_body_class' ) );
-?>
